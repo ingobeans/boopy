4e495ce3da8290a3fcb515d0b23242d396dcb2c5
ingobeans <127751491+ingobeans@users.noreply.github.com>
2024-05-19 20:39:25 +0200

    Update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='boopy',
-    version='0.3.2',
+    version='0.3.3',
     packages=find_packages(),
     package_data={
         'boopy': ['monobit.ttf','icon.png'],
