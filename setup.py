4504e9b9c71740d153ef1d4ac5e13d8d1d275daf
ingobeans <127751491+ingobeans@users.noreply.github.com>
2024-07-30 23:59:58 +0200

    made pygame and setuptools install with boopy

--- setup.py
+++ setup.py
@@ -2,10 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='boopy',
-    version='0.3.3',
+    version='0.3.4',
     packages=find_packages(),
     package_data={
         'boopy': ['monobit.ttf','icon.png'],
     },
     exclude_package_data={'': ['example.py']},
+    install_requires=[
+        "pygame-ce>=2.5.0",
+        "setuptools>=72.1.0",
+    ],
 )
